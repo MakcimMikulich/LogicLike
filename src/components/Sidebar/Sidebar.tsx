71ce0e41687e63a1f6a26fa293fa9ad39246a9e4
Makcik303123 <adoberoflit@gmail.com>
2024-05-28 22:18:54 +0300

    complete

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,35 +1,31 @@
-import { useCallback } from "react";
 import styles from "./Sidebar.module.scss";
 import type { SidebarProps } from "./types";
 
 export const Sidebar = ({
-	uniqueTags,
-	activeLink,
-	setActiveLink,
+  uniqueTags,
+  activeLink,
+  setActiveLink,
 }: SidebarProps) => {
-	const handlerClick = useCallback(
-		(index: number) => {
-			if (activeLink === index) {
-				return;
-			}
+  const handlerClick = (index: number) => {
+    if (activeLink === index) {
+      return;
+    }
 
-			setActiveLink(index);
-		},
-		[activeLink]
-	);
+    setActiveLink(index);
+  };
 
-	return (
-		<div className={styles.sidebar}>
-			{["Все темы", ...uniqueTags].map((link, index) => (
-				<div
-					data-active={activeLink === index}
-					key={index}
-					onClick={() => handlerClick(index)}
-					className={styles.sidebar__link}
-				>
-					{link}
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className={styles.sidebar}>
+      {["Все темы", ...uniqueTags].map((link, index) => (
+        <div
+          data-active={activeLink === index}
+          key={index}
+          onClick={() => handlerClick(index)}
+          className={styles.sidebar__link}
+        >
+          {link}
+        </div>
+      ))}
+    </div>
+  );
 };
